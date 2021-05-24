9ec8df41769d2cd2ad330caa40dd026633bd4bfd
In Tae woo <81672260+InTaewoo@users.noreply.github.com>
2021-05-24 17:42:32 +0900

    Add files via upload

---
+++ python/12_function/function_1.py
@@ -0,0 +1,29 @@
+#함수의 개요
+
+#함수 정의
+
+# def printName(name):
+#     print(name)
+# def area_sqaure(width,height):
+#     area = width * height
+#     return area
+# w=10
+# h=20
+# #함수 호출
+# area = area_sqaure(w,h)
+# print(area)
+# printName('Lee')
+
+#연습문제
+def show_info():
+    print('인태우\n27\n01012341234')
+show_info()
+
+#함수이름 sum
+#숫자 2개를 키보드로 입력받아서 두 수의 합을 출력
+def sum():
+    a=int(input('숫자 1 :'))
+    b=int(input('숫자 2 :'))
+    hab=a+b
+    print(hab)
+sum()
