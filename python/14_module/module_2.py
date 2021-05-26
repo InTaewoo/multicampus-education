bcf121df1d5901aba893b7701600708240b8c74f
In Tae woo <81672260+InTaewoo@users.noreply.github.com>
2021-05-26 22:51:39 +0900

    Add files via upload

---
+++ python/14_module/module_2.py
@@ -0,0 +1,8 @@
+#calculator module 참조
+import calculator
+a= calculator.add(9, 8)
+print(a)
+
+a= calculator.add(9, 8)
+print(a)
+showInfo.show_name()
---
+++ python/module_1.py
@@ -0,0 +1,18 @@
+import random
+import sys
+
+bmodule = sys.builtin_module_names
+print(bmodule)
+
+#dir(_builins_)
+
+#module 참조 : import
+import math
+# import random as rd 모듈 전체 / 별칭
+# from random import randint as rd 모듈 일부
+print(randint(1,10))
+
+# rd.randint(1,10)
+# random.randint(1,10)
+
+#계산기 함수 : add(x,y), sub(x,y), mul(x,y), div(x,y)
